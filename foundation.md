c845e75d01af78e530133eb5baeaa3895af279db
NGUYEN Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2024-01-12 22:59:58 +0100

    update

---
+++ foundation.md
@@ -0,0 +1,18 @@
+#Variable type
+int, float, complex
+
+#Operatior
+
+x % y: chia lay du
+x ** y: tuong duong x^y
+x // y: x chia cho y va lay phan nguyen
+
+#Swap 2 variable
+a, b= b, a
+
+#input
+luon tra ve string
+
+#map
+vi du ep kieu a= ['100', '200','300'] sang int va gan vao 3 bien x y z
+x, y, z = map(int, a)
---
+++ index.py
@@ -0,0 +1,23 @@
+from math import *
+import math
+# s = input('enter a number:')
+# print(f"number is {s}")
+# -----------------------
+# a = ['100', '200', '300']
+# x, y, z = map(int, a)
+
+# print(x,y,z)
+# -----------------------
+
+# print(sqrt(9))
+# print(isqrt(39)) #lay so nguyen cua can bac 2
+
+# -----------------------
+# a, b = 1000, 200
+# x = 'a<b' if a < b else 'a>b'
+# print(x)
+# -----------------------
+# Ham range(start,stop,step)
+a = range(1, 10, 1)
+for i in a:
+    print(i,end=' ')
